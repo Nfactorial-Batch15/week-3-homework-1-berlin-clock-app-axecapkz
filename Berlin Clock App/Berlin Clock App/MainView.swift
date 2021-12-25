d085a66f3b25ea2dd6e3ee8d6bbbace57fff4a0b
Azamat Kenjebayev <educationonline@me.com>
2021-12-26 02:38:56 +0600

    Fixed final version

--- Berlin Clock App/Berlin Clock App/MainView.swift
+++ Berlin Clock App/Berlin Clock App/MainView.swift
@@ -11,7 +11,7 @@ struct MainView: View {
     @State private var timePicker = Date()
     @State var berlinDate: [String] = []
     @State var normalClock: String = ""
-       
+    
     let rowShape =  RoundedRectangle(cornerRadius: 16, style: .continuous)
     
     var body: some View {
@@ -20,9 +20,8 @@ struct MainView: View {
             CustomColor.bgColorOriginal
                 .edgesIgnoringSafeArea(.all)
             VStack(alignment: .center, spacing: 0){
-                CustomTitle(text: "Time is \(timePicker.formatted(date: .omitted, time: .standard))")
-
-//                CustomTitle(text: "Time is \($timePicker)")
+                //                CustomTitle(text: "Time is \(timePicker.formatted(date: .omitted, time: .shortened))")
+                CustomTitle(text: "Time is \(normalClock)")
                     .padding(.top, 70)
                 ZStack{
                     MainFrame()
@@ -30,8 +29,8 @@ struct MainView: View {
                         if(berlinDate.count>0){
                             secondsRow
                             fiveHoursRow
-                            fiveMinutesRow
                             oneHourRow
+                            fiveMinutesRow
                             oneMinuteRow
                         }
                     }
@@ -44,10 +43,7 @@ struct MainView: View {
                             .frame(width: 190, height: 24, alignment: .leading)
                             .padding(.leading, 32)
                         ZStack{
-                            datePicker(timePicker: $timePicker)
-                                .onChange(of: timePicker, perform: { newValue in
-                                    showTime()
-                                })
+                            DatePicker("", selection: $timePicker, displayedComponents: .hourAndMinute)
                                 .environment(\.locale, Locale(identifier: "ru_RU"))
                                 .padding(.trailing, 32)
                         }
@@ -62,79 +58,69 @@ struct MainView: View {
     
     var secondsRow: some View {
         HStack{
-            Circle()
+                Circle()
                 .fill(berlinDate[0] == "Y" ? CustomColor.yellowOn : CustomColor.yellowOff)
                 .frame(width: 56, height: 56, alignment: .center)
-            //            }
         }
     }
     
-    var fiveHoursRow: some View {
-        HStack{
-            ForEach((1..<5)) {i in
-                rowShape
-                    .fill(berlinDate[i] == "R" ? CustomColor.redOn : CustomColor.redOff)
-                    .frame(width: CustomShapes.hmRowWidth, height: CustomShapes.hmRowHeight)
-            }
+var fiveHoursRow: some View {
+    HStack{
+        ForEach((1..<5)) {i in
+            rowShape
+                .fill(berlinDate[i] == "R" ? CustomColor.redOn : CustomColor.redOff)
+                .frame(width: CustomShapes.hmRowWidth, height: CustomShapes.hmRowHeight)
         }
-    }
-    
-    var fiveMinutesRow: some View {
-        HStack {
-            ForEach((5..<9)) {i in
-              rowShape
-                    .fill(berlinDate[i] == "R" ? CustomColor.redOn : CustomColor.redOff)
-                    .frame(width: CustomShapes.hmRowWidth, height: CustomShapes.hmRowHeight)
-            }
-        }
-    }
-    
-    var oneHourRow: some View {
-        HStack {
-            Group {
-                ForEach((9..<20)) {i in
-                   rowShape
-                        .fill(berlinDate[i] == "Y" ? CustomColor.yellowOn : (berlinDate[i] == "R" ? Color.red : CustomColor.yellowOff))
-                        .frame(width: CustomShapes.minutesRowWidth, height: CustomShapes.minutesRowHeight)
-                }
-            }
-        }
-    }
-    
-    var oneMinuteRow: some View {
-        HStack {
-            ForEach((20..<24)) {i in
-                rowShape
-                    .fill(berlinDate[i] == "Y" ? CustomColor.yellowOn : CustomColor.yellowOff)
-                    .frame(width: CustomShapes.hmRowWidth, height: CustomShapes.hmRowHeight)
-            }
-        }
-    }
-    
-    func showTime(){
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
-            let date = Date()
-            let calendar = Calendar.current
-            let seconds = calendar.component(.second, from: date)
-            let minutes = calendar.component(.minute, from: date)
-            let hours = calendar.component(.hour, from: date)
-            let berlinClock = BerlinClock()
-            normalClock = "\(hours):\(minutes):\(seconds)"
-            berlinDate = berlinClock.berlinClock(hours: hours, minutes: minutes, seconds: seconds).map{String($0)}
-        })
-            .fire()
     }
 }
 
-struct datePicker: View {
-    @Binding var timePicker: Date
-    var body: some View {
-        DatePicker("", selection: $timePicker, displayedComponents: .hourAndMinute)
-    .environment(\.locale, Locale(identifier: "ru_RU"))
-    .labelsHidden()
-    .datePickerStyle(.compact)
+var oneHourRow: some View {
+    HStack {
+        ForEach((5..<9)) {i in
+            rowShape
+                .fill(berlinDate[i] == "R" ? CustomColor.redOn : CustomColor.redOff)
+                .frame(width: CustomShapes.hmRowWidth, height: CustomShapes.hmRowHeight)
+        }
     }
 }
+
+var fiveMinutesRow: some View {
+    HStack {
+        Group {
+            ForEach((9..<20)) {i in
+                rowShape
+                    .fill(berlinDate[i] == "Y" ? CustomColor.yellowOn : (berlinDate[i] == "R" ? Color.red : CustomColor.yellowOff))
+                    .frame(width: CustomShapes.minutesRowWidth, height: CustomShapes.minutesRowHeight)
+            }
+        }
+    }
+}
+
+var oneMinuteRow: some View {
+    HStack {
+        ForEach((20..<24)) {i in
+            rowShape
+                .fill(berlinDate[i] == "Y" ? CustomColor.yellowOn : CustomColor.yellowOff)
+                .frame(width: CustomShapes.hmRowWidth, height: CustomShapes.hmRowHeight)
+        }
+    }
+}
+
+func showTime(){
+    Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
+        let calendar = Calendar.current
+        let seconds = calendar.component(.second, from: .now)
+        let minutes = calendar.component(.minute, from: timePicker)
+        let hours = calendar.component(.hour, from: timePicker)
+        let berlinClock = BerlinClock()
+        normalClock = "\(hours):\(minutes):\(seconds)"
+        berlinDate = berlinClock.berlinClock(hours: hours, minutes: minutes, seconds: seconds)
+            .map{String($0)}
+    })
+        .fire()
+}
+}
+
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
